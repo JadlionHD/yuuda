76e7717354839bd2dcd3e0fe54fdbf63023d16c1
JadlionHD <theendermanblue@gmail.com>
2021-01-02 01:27:31 +0700

    fixed howgay cmd

--- src/commands/Fun/howgay.js
+++ src/commands/Fun/howgay.js
@@ -1,6 +1,11 @@
 module.exports.run = async (client, msg, args) => {
-  const howgay = Math.floor(Math.random() * 101);
-	msg.channel.createMessage(`You are {howgay} gay!`)
+  	let howgay = Math.floor(Math.random() * 101);
+	let say = args.join(" ");
+    let user = msg.mentions[0] || client.users.get(say) || msg.author;
+    if(user.id === "421307985827201024") return msg.channel.createMessage(`🏳️‍🌈 | ${user.username} is **0%** Gay!`);
+    if(user.id === "494219204610883594") return msg.channel.createMessage(`🏳️‍🌈 | ${user.username} is **0%** Gay!`);
+
+	msg.channel.createMessage(`🏳️‍🌈 | ${user.username} is **${howgay}** Gay!`);
 }
 
 module.exports.config = {
