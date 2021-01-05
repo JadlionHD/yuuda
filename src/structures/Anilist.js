a92b610baeaca218ec0f9c8f89ee58ca9edfc8c2
JadlionHD <theendermanblue@gmail.com>
2021-01-05 16:04:52 +0700

    Update package.json and Adding Anilist.js

---
+++ src/structures/Anilist.js
@@ -0,0 +1,84 @@
+const axios = require("axios");
+const baseURL = "https://graphql.anilist.co";
+
+module.exports = class anilist {
+	static searchAnime(name) {
+		return new Promise(function(resolve, reject) {
+			axios({
+				url: baseURL,
+				method: "post",
+				data: {
+					query: `
+query {
+  Media (search: "${name}", type: ANIME) {
+    id
+    episodes
+    type
+    description(asHtml: false)
+    averageScore
+    duration
+    source
+    status
+    popularity
+    isAdult
+    favourites
+    bannerImage
+    coverImage {
+      large
+    }
+    title {
+      romaji
+      native
+      userPreferred
+    }
+    genres
+    startDate {
+      year
+      month
+      day
+    }
+  }
+}
+`
+				}
+			}).then(result => {
+				resolve(result.data)
+			}).catch(err => {
+				reject(err)
+			})
+		})
+	}
+
+	static searchCharacter(name) {
+		return new Promise(function(resolve, reject) {
+			axios({
+				url: baseURL,
+				method: "post",
+				data: {
+					query: `
+query {
+  Character (search: "${name}") {
+    name {
+      first
+      last
+      full
+      native
+    }
+    image {
+      large
+      medium
+    }
+    description
+    siteUrl
+  }
+}
+`
+				}
+			}).then(result => {
+				resolve(result.data)
+			}).catch(err => {
+				reject(err)
+			})	
+		})
+	}
+}
